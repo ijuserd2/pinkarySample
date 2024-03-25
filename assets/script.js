314349a3746984194d1377c65ed78ab9960f27e0
kardelen <kardelen8759.college@gmail.com>
2024-03-25 13:05:22 +0300

    User sayfası eksik yerler tamamlandı

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,34 @@
 const navbar = document.getElementsByClassName("navbar")[0];
-const navbarbox = document.getElementsByClassName("navbar-box")[0];
+const navbarBox = document.getElementsByClassName("navbar-box")[0];
+const addLinkButton = document.getElementsByClassName("setting-1")[0];
+const addLink = document.getElementsByClassName("setting-new-link")[0];
+const userPageSettingButton = document.getElementsByClassName("setting-2")[0];
+const userPageSetting = document.getElementsByClassName("setting-userpage")[0];
 navbar.addEventListener("click",function(){
-    if(navbarbox.style.display=="block"){
-        navbarbox.style.display="none";
+    if(navbarBox.style.display=="block"){
+        navbarBox.style.display="none";
     }else{
-        navbarbox.style.display="block";
+        navbarBox.style.display="block";
+    }
+})
+
+userPageSettingButton.addEventListener("click",function(){
+    if(addLink.style.display=="block"){
+        addLink.style.display="none";
+    }
+    if(userPageSetting.style.display=="block"){
+        userPageSetting.style.display="none";
+    }else{
+        userPageSetting.style.display="block";
+    }
+})
+addLinkButton.addEventListener("click",function(){
+    if(userPageSetting.style.display=="block"){
+        userPageSetting.style.display="none";
+    }
+    if(addLink.style.display=="block"){
+        addLink.style.display="none";
+    }else{
+        addLink.style.display="block";
     }
 })
