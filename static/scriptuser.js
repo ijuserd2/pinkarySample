e8b2791cb2eda9ee25e1571a35013e694a334c15
kardelen <kardelen8759.college@gmail.com>
2024-04-01 12:03:43 +0300

    user page add link update

--- static/scriptuser.js
+++ static/scriptuser.js
@@ -12,7 +12,6 @@ const sendLink = document.getElementById("sendLink");
 validRegex = /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?\/?$/gm;
 isLinkFormValid = false, isSettingFormValid = false;
 
-
 sendLink.addEventListener("click", () => {
     if(isLinkFormValid){
         linkForm.submit()
