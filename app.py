5e733887309d4bd6d20b2c2653c8c7b52ac21292
kardelen <kardelen8759.college@gmail.com>
2024-04-03 14:53:56 +0300

    session

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 # coding: utf8
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import dbsql
 import page_settings
 
 app = Flask(__name__)
-
+app.secret_key = "super secret key"
 @app.route('/', methods=['GET','POST'])
 def index():
     return render_template('index.html')
 
 @app.route('/home/', methods=['GET','POST'])
 def home():
-    return render_template('home.html')
-
+    if 'email' in session:
+        return render_template('home.html', email=session['email'])
+    return redirect(url_for('login'))
 @app.route('/login/', methods=['GET','POST'])
 def login():
     if request.method == "GET":
@@ -23,23 +24,39 @@ def login():
         password = request.form.get("pwd")
         check = dbsql.email_pass_check(email, password)
         if(check!=None):
-            hello_user = "Hello" + dbsql.fetch_user_fullname(email, password)+"!"
-            return redirect(url_for('home',hello_user="hello_user"))
+            session['email'] = email
+            userid = dbsql.fetch_user_id(email)
+            session['userid'] = userid
+            return redirect(url_for('home'))
         return redirect(url_for('login'))
+
+@app.route('/u/<username>', methods=['GET','POST'])
+def u(username):
+
+    userid = dbsql.fetch_user_id_by_username(username)
+    userid = str(userid)
+    check_username = dbsql.fetch_user_by_username(username)
+    if(check_username!=None):
+        try:
+            linkdata = dbsql.fetch_test_links(userid)
+            settingsvalues = dbsql.fetch_test_user_pagesettings(userid)
+            return render_template('user.html', bgcolor=settingsvalues[0], borderradius=settingsvalues[1],
+                                   linkdata=linkdata)
+        except Exception:
+            pass
+    return redirect(url_for('home'))
 
 @app.route('/register/', methods=['GET','POST'])
 def register():
     if request.method == "GET":
         return render_template('register.html')
     if request.method == "POST":
-
         full_name = request.form.get("fullname")
         username = request.form.get("username")
         email = request.form.get("email")
         password = request.form.get("pwd")
         dbsql.sign_up(full_name,username,email,password)
-
-        return redirect(url_for('home'))
+        return redirect(url_for('login'))
 
 @app.route('/user/', methods=['GET','POST'])
 def user():
@@ -71,27 +88,35 @@ def user():
         settingsvalues = dbsql.fetch_test_user_pagesettings()
         return render_template('user.html', bgcolor=settingsvalues[0], borderradius=settingsvalues[1], linkdata = linkdata)
 
+@app.route('/logout/', methods=['GET','POST'])
+def logout():
+    session.pop('email', None)
+    session.pop('userid', None)
+    return redirect(url_for('login'))
 @app.route('/profile/', methods=['GET','POST'])
 def profile():
-    if request.method == "GET":
-        return render_template('profile.html')
-    if request.method == "POST":
-        if (request.form["update_acc"] == "acc"):
-            name = request.form.get("fullname")
-            username = request.form.get("username")
-            email = request.form.get("email")
-            bio = request.form.get("bio")
-            dbsql.update_test_user_acc(name, username, email, bio)
+    if 'email' in session:
+        if request.method == "GET":
+            return render_template('profile.html')
+        if request.method == "POST":
+            if (request.form["update_acc"] == "acc"):
+                name = request.form.get("fullname")
+                username = request.form.get("username")
+                email = request.form.get("email")
+                bio = request.form.get("bio")
+                dbsql.update_test_user_acc(name, username, email, bio)
+
+                return render_template('profile.html')
+            if (request.form["update_acc"] == "pwd"):
+                old_pwd = request.form.get("old_pwd")
+                new_pwd = request.form.get("new_pwd")
+                c_new_pwd = request.form.get("c_new_pwd")
+                dbsql.update_test_user_pass(old_pwd, new_pwd, c_new_pwd)
+                return render_template('profile.html')
 
             return render_template('profile.html')
-        if (request.form["update_acc"] == "pwd"):
-            old_pwd = request.form.get("old_pwd")
-            new_pwd = request.form.get("new_pwd")
-            c_new_pwd = request.form.get("c_new_pwd")
-            dbsql.update_test_user_pass(old_pwd, new_pwd, c_new_pwd)
-            return render_template('profile.html')
 
-        return render_template('profile.html')
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
     app.run(debug=True)
--- dbsql.py
+++ dbsql.py
@@ -14,6 +14,7 @@ def sign_up(name, username, email, pwd):
     sign_up_sql = "INSERT INTO Users(FullName, UserName, Email, Pass, Bio, DataCreated, DataModified) VALUES ('"+name+"', '"+username+"', '"+email+"', '"+pwd+"', '', GETDATE(), GETDATE())"
     cursor.execute(sign_up_sql)
     cursor.commit()
+    cursor.close()
 def email_pass_check(email, pwd):
     email_pass_check_sql = "SELECT * FROM Users WHERE Email = '"+email+"' AND Pass ='"+pwd+"' COLLATE Latin1_General_CS_AS"
     cursor.execute(email_pass_check_sql)
@@ -25,46 +26,83 @@ def fetch_user_pass(email):
     cursor.execute(fetch_user_pass_sql)
     dbpass = cursor.fetchall()[0][0]
     return dbpass
+
+def fetch_user_by_username(username):
+    fetch_user_sql = "SELECT * FROM Users WHERE UserName = '"+username+"'"
+    cursor.execute(fetch_user_sql)
+    dbuser = cursor.fetchone()
+    return dbuser
+def fetch_user_id(email):
+    fetch_user_id_sql = "SELECT UserId FROM Users WHERE Email = '"+email+"'"
+    cursor.execute(fetch_user_id_sql)
+    dbid = cursor.fetchall()[0][0]
+    return dbid
+
+def fetch_user_id_by_username(username):
+    fetch_user_id_sql = "SELECT UserId FROM Users WHERE UserName = '"+username+"'"
+    cursor.execute(fetch_user_id_sql)
+    try:
+        dbid = cursor.fetchall()[0][0]
+    except Exception:
+        dbid = None
+        pass
+    return dbid
 def fetch_user_pass_by_id():
     fetch_user_pass_sql = "SELECT Pass FROM Users WHERE UserId = 11 "
     cursor.execute(fetch_user_pass_sql)
     dbpass = cursor.fetchall()[0][0]
     return dbpass
-def fetch_user_fullname(email, pwd):
-    fetch_user_pass_sql = "SELECT FullName FROM Users WHERE Email = '"+email+"' AND Pass ='"+pwd+"' COLLATE Latin1_General_CS_AS"
+def fetch_user_fullname(userid):
+    fetch_user_pass_sql = "SELECT FullName FROM Users WHERE UserId ='"+userid+"' COLLATE Latin1_General_CS_AS"
     cursor.execute(fetch_user_pass_sql)
     db_full_name = cursor.fetchone()[0]
+    print(db_full_name)
     return db_full_name
-def fetch_test_user_pagesettings():
-    fetch_sql = "SELECT BgColor FROM UserPageSettings WHERE UserId = 11"
+def fetch_test_user_pagesettings(userid):
+    fetch_sql = "SELECT BgColor FROM UserPageSettings WHERE UserId ='"+userid+"'"
     cursor.execute(fetch_sql)
     db_bgcolor = cursor.fetchone()[0]
-    fetch_sql = "SELECT BorderRadius FROM UserPageSettings WHERE UserId = 11"
+    fetch_sql = "SELECT BorderRadius FROM UserPageSettings WHERE UserId = '"+userid+"'"
     cursor.execute(fetch_sql)
     db_border_radius = cursor.fetchone()[0]
     return [db_bgcolor, db_border_radius]
 
-def set_test_user_pagesettings(bgcolor, borderradius):
-    set_sql = "UPDATE UserPageSettings SET BgColor = '" + bgcolor + "', BorderRadius = '" + borderradius + "' WHERE UserId = 11"
+def set_test_user_pagesettings(bgcolor, borderradius, userid):
+    set_sql = "UPDATE UserPageSettings SET BgColor = '" + bgcolor + "', BorderRadius = '" + borderradius + "' WHERE UserId = '"+userid+"'"
     cursor.execute(set_sql)
     cursor.commit()
-def fetch_test_links():
-    fetch_sql = "SELECT * FROM ProjectDB.dbo.Links WHERE UserId = 11"
+def fetch_test_links(userid):
+    fetch_sql = "SELECT * FROM ProjectDB.dbo.Links WHERE UserId = '"+userid+"'"
     cursor.execute(fetch_sql)
     db_link_data = cursor.fetchall()
     return db_link_data
 
-def set_test_links(desc, link):
-    set_sql = "INSERT INTO Links(UserId, Description, Link, DataCreated, DataModified) VALUES (11, '"+desc+"', '"+link+"', GETDATE(), GETDATE())"
+def fetch_pagesettings_by_username(username):
+    fetch_sql = "SELECT BgColor FROM UserPageSettings WHERE UserId ='"+username+"'"
+    cursor.execute(fetch_sql)
+    db_bgcolor = cursor.fetchone()[0]
+    fetch_sql = "SELECT BorderRadius FROM UserPageSettings WHERE UserId = '"+username+"'"
+    cursor.execute(fetch_sql)
+    db_border_radius = cursor.fetchone()[0]
+    return [db_bgcolor, db_border_radius]
+
+def fetch_links_by_username(username):
+    fetch_sql = "SELECT * FROM ProjectDB.dbo.Links WHERE UserName = '"+username+"'"
+    cursor.execute(fetch_sql)
+    db_link_data = cursor.fetchall()
+    return db_link_data
+
+def set_test_links(desc, link, userid):
+    set_sql = "INSERT INTO Links(UserId, Description, Link, DataCreated, DataModified) VALUES ('"+userid+"', '"+desc+"', '"+link+"', GETDATE(), GETDATE())"
     cursor.execute(set_sql)
     cursor.commit()
 
-def update_test_user_acc(name, username, email, bio):
-    update_sql = "UPDATE Users SET FullName = '" + name + "', UserName = '" + username + "', Email = '" + email + "', Bio = '" + bio + "', DataModified = GETDATE() WHERE UserId = 11"
+def update_test_user_acc(name, username, email, bio, userid):
+    update_sql = "UPDATE Users SET FullName = '" + name + "', UserName = '" + username + "', Email = '" + email + "', Bio = '" + bio + "', DataModified = GETDATE() WHERE UserId = '"+userid+"'"
     cursor.execute(update_sql)
     cursor.commit()
 
-def update_test_user_pass(old_pwd, new_pwd, c_new_pwd):
+def update_test_user_pass(old_pwd, new_pwd, c_new_pwd, userid):
     old_pwd_fetched = fetch_user_pass_by_id()
     if(old_pwd != old_pwd_fetched):
         print("old pwd wrong")
@@ -75,7 +113,7 @@ def update_test_user_pass(old_pwd, new_pwd, c_new_pwd):
     if (old_pwd == new_pwd):
         print("old pwd same with new pwd")
         return False
-    update_sql = "UPDATE Users SET Pass = '" + new_pwd + "', DataModified = GETDATE() WHERE UserId = 11"
+    update_sql = "UPDATE Users SET Pass = '" + new_pwd + "', DataModified = GETDATE() WHERE UserId = '"+userid+"'"
     cursor.execute(update_sql)
     cursor.commit()
 
